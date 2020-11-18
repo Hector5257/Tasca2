65e48638abd497694f67e5ec88a3375cf4dac06a
Hector5257 <hjdm5257@gmail.com>
2020-11-18 17:48:50 +0100

    F

---
+++ src/main/java/org/openjfx/Ejercicio01.java
@@ -0,0 +1,13 @@
+package org.openjfx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class Ejercicio01 {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("Tasca2");
+
+    }
+}
---
+++ src/main/java/org/openjfx/Tasca2.java
@@ -0,0 +1,52 @@
+package org.openjfx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class Tasca2 {
+
+    @FXML
+    private void switchtoEj1() throws IOException {
+        App.setRoot("Ejercicio1");
+
+    }
+
+    @FXML
+    private void switchtoEj2() throws IOException {
+        App.setRoot("Ejercicio2");
+
+    }
+
+    @FXML
+    private void switchtoEj3() throws IOException {
+        App.setRoot("Ejercicio3");
+
+    }
+
+    @FXML
+    private void switchtoEj4V() throws IOException {
+        App.setRoot("Ejercicio4V");
+
+    }
+
+    @FXML
+    private void switchtoEj4H() throws IOException {
+        App.setRoot("Ejercicio4H");
+
+    }
+
+
+    @FXML
+    private void switchtoEj5() throws IOException {
+        App.setRoot("Ejercicio5");
+
+    }
+
+
+    @FXML
+    private void switchtoEj6() throws IOException {
+        App.setRoot("Ejercicio6");
+
+    }
+
+}
